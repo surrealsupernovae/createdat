6f4bd104a09d728e3f8009270d7845ab75843110
surrealsupernovae <surrealsupernovae@disr.it>
2024-09-26 12:14:51 +0200

    added README

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-/// Creates .dat files of different sizes based on arguments given in the command line
-/// The first argument is the number of a certain unit that you would like, it is supposed to be a unsigned integer.
-/// The second argument corresponds to the unit, there are only three right now: KB, MB and GB.
-/// The file will be named "size_unit.dat"
-/// # Example
-/// ```
-/// cargo run -- 20 KB
-/// ```
+//! Creates .dat files of different sizes based on arguments given in the command line
+//! The first argument is the number of a certain unit that you would like, it is supposed to be a unsigned integer.
+//! The second argument corresponds to the unit, there are only three right now: KB, MB and GB.
+//! The file will be named "size_unit.dat"
+//! # Example
+//! ```
+//! cargo run -- 20 KB
+//! ```
 
 use std::env;
 use std::fs;
